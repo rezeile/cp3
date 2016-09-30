9d8473827d4f0feecba27192010e49e515e799ef
Eliezer Abate <code.eliezer@gmail.com>
2016-09-30 15:46:13 -0700

    reasonable looking output is begin generated fro the sum it up problem

--- backtracking/sum-it-up.cc
+++ backtracking/sum-it-up.cc
@@ -1,25 +1,49 @@
-#include <vector>
 #include <set>
+#include <vector>
 #include <iostream>            // for cout,cin
 #include <cstdio>              // for freopen
+#include <iterator>
 using namespace std;
-typedef vector<vector<int>> vii;
+typedef set<vector<int>> svi;
 
-void printResult(vii &result) {
+void printResult(svi &result) {
     if(result.size() == 0) {
         cout << "NONE\n"; return;
     }
-    for(int i = 0; i < result.size(); i++) {
-        vector<int> v = result[i];
+    set<vector<int>>::reverse_iterator i;
+    for(i = result.rbegin(); i != result.rend(); i++) {
+        vector<int> v = *i;
         for(int j = 0; j < v.size(); j++) {
             if(j != v.size() - 1) cout << v[j] << "+";
             else cout << v[j] << "\n";
         }
     }
 }
+void sumItUpHelper(int total,svi &v,vector<int> &candidate_set,vector<int> input_set) {
+    if(total < 0 || input_set.size() <= 0) return;
+    else if(total == 0) {
+        vector<int> res(candidate_set);
+        v.insert(res);
+    } else {
+       for(int i = 0; i < input_set.size(); i++) {
+            vector<int> input_sset(input_set);
+            int val = input_sset[i];
+            input_sset.erase(input_sset.begin() + i);
+            candidate_set.push_back(val);
+            sumItUpHelper(total - val,v,candidate_set,input_sset);
+            candidate_set.pop_back();
+            input_sset.insert(input_sset.begin() + i,val);
+       }
+    }
+}
 
-vii sumItUp(int total,multiset<int> s) {
-    vii v;
+svi sumItUp(int total,vector<int> s) {
+    svi v;
+    vector<int>::iterator i;
+    for(i = s.begin(); i != s.end(); i++) {
+        vector<int> sum_set;
+        sumItUpHelper(*i,v,sum_set,s);
+    }
     return v;
 }
 
@@ -32,14 +56,14 @@ int main(int argc, char *argv[]) {
     while(cin >> total) {
         if(!total) break; // done 
         cin >> N;
-        multiset<int> s;
+        vector<int> s;
         int val;
         printf("Sums of %d:\n",total);
         for(int i = 0; i < N; i++) {
             cin >> val;
-            s.insert(val);
+            s.push_back(val);
         }
-        vii result = sumItUp(total,s);
+        svi result = sumItUp(total,s);
         printResult(result);
     }
     return 0;
