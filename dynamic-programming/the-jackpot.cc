4acfdf094b99e9000ee8cf952cfda5e2a1701803
Eliezer Abate <code.eliezer@gmail.com>
2016-10-03 11:42:52 -0700

    finished maximum 1d sum (i.e. range minimum query)

---
+++ dynamic-programming/the-jackpot.cc
@@ -0,0 +1,54 @@
+/* UVa 10684: The jackpot
+ * 
+ * Author: Eliezer Abate
+ * Last Updated: October 3, 2016
+ */
+
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int maxWinningStreak(vector<int> &arr) {
+    int running_sum = 0, mws = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        if(running_sum + arr[i] >= 0) {
+            running_sum += arr[i];
+            mws = max(mws,running_sum);
+        } else running_sum = 0;
+    }
+    return mws;
+}
+
+void printArray(vector<int> v) {
+    for(int i = 0; i < v.size(); i++) {
+        if(i != v.size() - 1) cout << v[i] << ",";
+        else cout << v[i] << endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    /*if(argc < 2) {
+        cerr << "enter an input file." << endl; return -1;
+    }*/
+    // obtain input
+    //freopen(argv[1],"r",stdin);
+    int N;
+    while(true) {
+        cin >> N;
+        if(N == 0) break;
+        vector<int> arr;
+        int val;
+        for(int i = 0; i < N; i++) {
+            cin >> val;
+            arr.push_back(val);
+        }
+        //printArray(arr);
+        int mws = maxWinningStreak(arr);
+        string s = mws > 0 ? "The maximum winning streak is " + to_string(mws) + "." : "Losing streak.";
+        cout << s << endl; 
+    }
+    return 0;
+}
--- dynamic-programming/top-coder-tutorials/avoid-roads.cc
+++ dynamic-programming/top-coder-tutorials/avoid-roads.cc
@@ -1,19 +1,20 @@
 #include <vector>           // for vector
 #include <iostream>         // for cout
-#include <unordered_map>    // for unordered_multimap
-#include <utitlity>         // for pair
+#include <set>              // set
+#include <string>
 using namespace std;
 
 typedef long long ll;
+
 ll numWays(int width,int height,vector<string> &bad) {
-    unordered_multimap<int> restricted;
+    set<string> s(bad.begin(),bad.end());
     ll grid[height + 1][width + 1];
     for(int i = 0; i <= height; i++) {
         for(int j = 0; j <= width; j++) {
             if(i == 0 || j == 0) {
                 grid[i][j] = 1;
             } else {
-                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];    
             }
         }
     }
@@ -21,7 +22,8 @@ ll numWays(int width,int height,vector<string> &bad) {
 }
 
 int main() {
-    ll n = numWays(3,3);
+    vector<string> v = {"0 0 0 1","6 6 5 6"};
+    ll n = numWays(6,6,v); 
     cout << n << endl;
     return 0;
 }
