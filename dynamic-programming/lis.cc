5c65d126c390cee1b992d24c7e5ea6de3b8dffcd
Eliezer Abate <code.eliezer@gmail.com>
2016-10-03 16:41:57 -0700

    completed longest increasing subsequence with just lenght and not reconstruction

--- dynamic-programming/lis.cc
+++ dynamic-programming/lis.cc
@@ -6,7 +6,35 @@
  * Last Updated October 3 2016
  *
  */
+ 
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
 
- int main() {
+void printLongestIncreasingSubsequence(vector<int> &v) {
+    if(v.size() == 0) return;
+    vector<int> sl(v.size()); // subsequence length
+    sl[0] = 1;
+    int max_index = 0;
+    for(int i = 1; i < v.size(); i++) {
+        for(int j = 0; j < i; j++) {
+            if(sl[i] < (sl[j] + 1) && v[j] < v[i]) {
+                sl[i] = sl[j] + 1;
+                max_index = sl[i] >= sl[max_index] ? i : max_index;
+            }
+        }
+    }
+} 
+
+int main() {
+    vector<int> v;
+    int val;
+    while(cin >> val) {
+        v.push_back(val);
+    }
+    cout << v.size() << endl;
+    cout << "-" << endl;
+    printLongestIncreasingSubsequence(v);
     return 0;
- }
+}
