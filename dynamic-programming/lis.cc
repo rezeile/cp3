e1b3af3be91c6d4bb006ba4b244d9a2e1f7b55c2
Eliezer Abate <code.eliezer@gmail.com>
2016-10-03 17:35:12 -0700

    added some input files and finished longest increasing subsequence problem

--- dynamic-programming/lis.cc
+++ dynamic-programming/lis.cc
@@ -6,6 +6,9 @@
  * increasing subsequence of the array rather than simply
  * the length of the longest increasing subsequence.
  *
+ * The following implementation uses bottom-up (i.e. true 
+ * Dynamic Programming)
+ *
  * Author: Eliezer Abate
  * Last Updated October 3 2016
  *
@@ -18,32 +21,41 @@ using namespace std;
 
 void printLongestIncreasingSubsequence(vector<int> &v) {
     if(v.size() == 0) return;
-    vector<int> sl(v.size()); // subsequence length
+    // there is atleast a subsequence of size one for each element 
+    vector<int> sl(v.size(),1); // subsequence length
+    vector<vector<int>> subseq(v.size());
+    for(int i = 0; i < v.size(); i++) {
+        subseq[i].push_back(v[i]);
+    }
     sl[0] = 1;
     int max_index = 0;
     for(int i = 1; i < v.size(); i++) {
         for(int j = 0; j < i; j++) {
-            if(sl[i] < (sl[j] + 1) && v[j] < v[i]) {
+            if(sl[i] <= (sl[j] + 1) && v[j] < v[i]) {
                 sl[i] = sl[j] + 1;
+                subseq[i] = subseq[j]; subseq[i].push_back(v[i]);
                 max_index = sl[i] >= sl[max_index] ? i : max_index;
             }
         }
     }
-    cout << sl[max_index] << endl;
+    vector<int> lis = subseq[max_index];
+    cout << lis.size() << endl;
+    cout << "-" << endl;
+    for(int i = 0; i < lis.size(); i++) {
+        cout << lis[i] << endl;
+    }
 } 
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) {
+    /*if(argc < 2) {
         cerr << "enter an input file" << endl; return -1;
     }
-    freopen(argv[1],"r",stdin);
+    freopen(argv[1],"r",stdin);*/
     vector<int> v;
     int val;
     while(cin >> val) {
         v.push_back(val);
     }
-    cout << v.size() << endl;
-    cout << "-" << endl;
     printLongestIncreasingSubsequence(v);
     return 0;
 }
