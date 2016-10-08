55f5fe0c11620a6192609e07794698070b3778e6
Eliezer Abate <code.eliezer@gmail.com>
2016-10-07 18:51:13 -0700

    completed coding up the anagrams problem

--- util/testing.h
+++ util/testing.h
@@ -1,6 +1,7 @@
 #ifndef __TESTING_H
 #define __TESTING_H
 #include <iostream>
+#include <vector>
 using namespace std;
 
 template<typename T>
@@ -10,4 +11,13 @@ void compareOutput(T expected, T received) {
 	cout << "===============================" << endl;
 }
 
+template<typename Type>
+void printArray(vector<Type> &arr) {
+    cout << "[";
+    for(int i = 0; i < arr.size(); i++) {
+        if(i != arr.size() - 1) cout << arr[i] << ",";
+        else cout << arr[i]; 
+    }
+    cout << "]\n";
+}
 #endif
