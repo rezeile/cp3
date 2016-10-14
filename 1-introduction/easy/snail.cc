a230de2e8f405d9bb67b291040d3147500983991
Eliezer Abate <code.eliezer@gmail.com>
2016-10-14 08:18:52 -0700

    added edge test cases to snail

--- 1-introduction/easy/snail.cc
+++ 1-introduction/easy/snail.cc
@@ -6,7 +6,7 @@ void climb(double H,double U,double D, double F) {
    double ff = (F / 100.0) * U;
    double height = 0.0;
    int day = 1;
-   while(U > 0.0) {
+   while(U >= 0.0) {
         height += U;
         if(height > H) {printf("success on day %d\n",day); return; };
         height -= D;
@@ -14,13 +14,12 @@ void climb(double H,double U,double D, double F) {
         U -= ff;
         day++;; 
    }
-   day += 1;
    printf("failure on day %d\n",day);
 }
 
 int main(int argc, char *argv[]) {
-    //if(argc < 2) { cerr << "enter an input file"; return -1; }
-    //freopen(argv[1],"r",stdin);
+    if(argc < 2) { cerr << "enter an input file"; return -1; }
+    freopen(argv[1],"r",stdin);
     
     double H,U,F,D; 
     while(cin >> H) {
