76b765ab30bc978f50dda2c197e1ed03605fd451
Eliezer Abate <code.eliezer@gmail.com>
2016-10-13 21:41:13 -0700

    halfway through the snail problem

---
+++ 1-introduction/easy/snail.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void climb(double H,double U,double D, double F) {
+   double ff = (F / 100.0);
+   double height = 0;
+   int day = 1;
+   while(U > 0.0) {
+        height += (U - D);
+        if(height > H){ printf("success on day %d\n",day); return;}
+        if(height < 0) {printf("failure on day %d\n",day); return;}
+        U -= ff;
+        day++;; 
+   }
+   printf("failure on day %d\n",day);
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) { cerr << "enter an input file"; return -1; }
+    freopen(argv[1],"r",stdin);
+    
+    double H,U,F,D; 
+    while(cin >> H) {
+        if(H == 0) break;
+        cin >> U >> F >> D;
+        climb(H,U,F,D);
+    }
+    return 0;
+}
