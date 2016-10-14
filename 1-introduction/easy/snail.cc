19b9cf65bf2460838de0ed989fd7725d7525691e
Eliezer Abate <code.eliezer@gmail.com>
2016-10-14 08:09:10 -0700

    have snail examples passing

--- 1-introduction/easy/snail.cc
+++ 1-introduction/easy/snail.cc
@@ -3,22 +3,24 @@
 using namespace std;
 
 void climb(double H,double U,double D, double F) {
-   double ff = (F / 100.0);
-   double height = 0;
+   double ff = (F / 100.0) * U;
+   double height = 0.0;
    int day = 1;
    while(U > 0.0) {
-        height += (U - D);
-        if(height > H){ printf("success on day %d\n",day); return;}
-        if(height < 0) {printf("failure on day %d\n",day); return;}
+        height += U;
+        if(height > H) {printf("success on day %d\n",day); return; };
+        height -= D;
+        if(height < 0) {printf("failure on day %d\n",day); return; };
         U -= ff;
         day++;; 
    }
+   day += 1;
    printf("failure on day %d\n",day);
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) { cerr << "enter an input file"; return -1; }
-    freopen(argv[1],"r",stdin);
+    //if(argc < 2) { cerr << "enter an input file"; return -1; }
+    //freopen(argv[1],"r",stdin);
     
     double H,U,F,D; 
     while(cin >> H) {
--- util/uva-template.cc
+++ util/uva-template.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) { cerr << "enter an input file;" return -1; }
+    if(argc < 2) { cerr << "enter an input file"; return -1; }
     freopen(argv[1],"r",stdin);
     return 0;
 }
