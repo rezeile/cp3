add8ef899ea4d0c6053afe65c0cb5c8937a74f60
Eliezer Abate <code.eliezer@gmail.com>
2016-10-16 18:52:26 -0700

    finished ananagrams

--- 1-introduction/anagram/ananagrams.cc
+++ 1-introduction/anagram/ananagrams.cc
@@ -1,29 +1,31 @@
-#include <iostream>
-#include <cstdio>
-#include <sstream> // for std::sstream
-#include <string>       // for std::string
-#include <vector>
-#include <set>
-#include <cctype>
+#include <iostream> // for cout, cin, cerr
+#include <cstdio>   // for freopen
+#include <string>   // for std::string
+#include <vector>   // for vector
+#include <set>      // for multiset
+#include <cctype>   // for tolower
+#include <algorithm> // for sort
 using namespace std;
 
 bool isAnagram(string a, string b) {
     if(a.size() == b.size()) {
+        string s1 = "",s2 = "";
         for(int i = 0; i < a.size(); i++) {
-            if(tolower(a[i]) != tolower(b[i])) return false;
+            s1 += tolower(a[i]);
+            s2 += tolower(b[i]);
         }
-        return true;
+        sort(s1.begin(),s1.end()); sort(s2.begin(),s2.end());
+        return s1 == s2;
     }
     return false;
 }
-bool isAnanagram(string s, multiset<string> &words,multiset<string>::iterator it) {
-    string s1 = s,s2;
-    sort(s1.begin(),s1.end());
+bool isAnanagram(string s,multiset<string> &words,multiset<string>::iterator it) {
     for(auto i = words.begin(); i != words.end(); i++) {
         if(i == it) continue;
-        s2 = *i;
-        sort(s2.begin(),s2.end());
-        if(isAnagram(s1,s2)) return false;
+        string s2 = *i;
+        if(isAnagram(s,s2)) {
+            return false;
+        }
     }
     return true;
 }
@@ -31,16 +33,14 @@ bool isAnanagram(string s, multiset<string> &words,multiset<string>::iterator it
 void printAnanagrams(multiset<string> words) {
     for(auto i = words.begin(); i != words.end(); i++) {
         string word = *i;
-        words.erase(i);
         if(isAnanagram(word,words,i))
             cout << word << endl;
-        words.insert(word);
     }
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) { cerr << "enter an input file"; return -1; }
-    freopen(argv[1],"r",stdin);
+    //if(argc < 2) { cerr << "enter an input file"; return -1; }
+    //freopen(argv[1],"r",stdin);
     string word;
     multiset<string> words;
     while(cin >> word) {
