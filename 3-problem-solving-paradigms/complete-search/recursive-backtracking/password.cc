3902850cf5fd0376703a0edf7eb2cecb946185dd
Eliezer Abate <code.eliezer@gmail.com>
2016-10-17 17:17:19 -0700

    added password.cc to Makefile

---
+++ 3-problem-solving-paradigms/complete-search/recursive-backtracking/password.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <set>          // for std::set
+#include <map>          // for std::map
+#include <vector>
+using namespace std;
+
+typedef vector<vector<char>> mtx;
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) { cerr << "enter an input file"; return -1; }
+    freopen(argv[1],"r",stdin);
+    // obtain input 
+    int T,k;
+    cin >> T >> k;
+    for(int i = 0; i < T; i++) {
+         
+    }
+    return 0;
+}
