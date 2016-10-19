ee0a5d5d0d58f5a4b7b84177eb7ef720574f2815
Eliezer Abate <code.eliezer@gmail.com>
2016-10-19 10:09:48 -0700

    forward emails gets a time limit at the moment

--- 4-graph/graph-traversal/test.cc
+++ 4-graph/graph-traversal/test.cc
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <cstdio>
-#include <cstdlib>
+#include <cstdlib> // srand,rand
+#include <ctime>   // time
+#include <unordered_set>
 using namespace std;
+
+void random(int N) {
+    freopen("input/forward-05.txt","w+",stdout);
+    int TC = 20,u,v;
+    printf("%d\n%d\n",TC,N);
+    unordered_set<int> s;
+    srand(time(NULL));
+    while(s.size() <= N) {
+        u  = rand() % N + 1;
+        v  = rand() % N + 1;
+        if(s.find(u) != s.end()) continue;
+        if(u == v) v--;
+        printf("%d %d\n",u,v);
+    }
+}
+
+void large(int N) {
+    freopen("input/forward-04.txt","w+",stdout);
+    int TC = 20,u,v;
+    printf("%d\n",TC);
+    for(int i = 0; i < TC; i++) {
+        printf("%d\n",N);
+        for(int i = 1; i <= N; i++) {
+            if(i != N) { u = i; v = i + 1;}
+            else {u = i; v = 1; }
+            printf("%d %d\n",u,v);
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
     if(argc < 2) { cerr << "enter an input integer." << endl; return 0; }
-    freopen("input/forward-03.txt","w+",stdout);
-    int TC = 1,u,v;
     int N = atoi(argv[1]);
-    printf("%d\n%d\n",TC,N);
-    for(int i = 1; i <= N; i++) {
-        if(i != N) { u = i; v = i + 1;}
-        else {u = i; v = 1; }
-        printf("%d %d\n",u,v);
-    }
+    large(N);
     return 0;
 }
