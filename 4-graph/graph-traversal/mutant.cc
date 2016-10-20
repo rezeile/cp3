1e29e3a44972cd17962489643acf1bfff83a233d
Eliezer Abate <code.eliezer@gmail.com>
2016-10-20 16:24:26 -0700

    finished testing for mutant graph traversal problem

--- 4-graph/graph-traversal/mutant.cc
+++ 4-graph/graph-traversal/mutant.cc
@@ -11,11 +11,11 @@ deque<char> d = {'N','E','S','W'}; // global
 
 char nextRotation(deque<char> &d,char dir) {
     if(dir == 'L') {
-        char front = d.front(); d.pop_front();
-        d.push_back(front);
-    } else {
         char back = d.back(); d.pop_back();
         d.push_front(back);
+    } else {
+        char front = d.front(); d.pop_front();
+        d.push_back(front);
     }
     return d.front();
 }
@@ -26,16 +26,16 @@ pair<int,int> nextCoordinates(int x,int y,int N,int M) {
     pair<int,int> p; p.first = p.second = -1;
     if(dir == 'N') {
         // increase y by 1
-        if(x <= N && (y + 1) <= M) { p.first = x; p.second = ++y; return p; }
+        if(x <= N && (y+1) <= M) { p.first = x; p.second = ++y; return p; }
     } else if (dir == 'S') {
         // decreate y by 1
-        if(x <= N && (y - 1) <= M) { p.first = x; p.second = --y; return p; }
+        if(x <= N && (y-1) >= 0) { p.first = x; p.second = --y; return p; }
     } else if (dir == 'E') {
         // increase x by 1
-        if(++x <= N && y <= M) { p.first = ++x; p.second = y; return p; }
+        if((x+1) <= N && y <= M) { p.first = ++x; p.second = y; return p; }
     } else {
         // decrease x by 1
-        if(--x <= N && y <= M) { p.first = --x; p.second = y; return p; }
+        if((x-1) >= 0 && y <= M) { p.first = --x; p.second = y; return p; }
     }
     return p;
 }
@@ -44,21 +44,21 @@ void endingPosition(string s,char dir,int sa,int sb,int N,int M,vbb &grid) {
    int cur_a = sa, cur_b = sb;
    pair<int,int> coor; coor.first = -1; coor.second = -1; 
    for(int i = 0; i < s.size(); i++) {
-       if(s[i] == 'L' || s[i] == 'R') dir = nextRotation(d,s[i]);
-       else {
+       if(s[i] == 'L' || s[i] == 'R') {
+            dir = nextRotation(d,s[i]);
+       } else {
             coor = nextCoordinates(cur_a,cur_b,N,M);         
             if(coor.first == -1 && grid[cur_a][cur_b]) continue; // scent smelled
             else if(coor.first == -1) { 
-                grid[cur_a][cur_b] = true; 
-                printf("%d %d %c LOST\n",cur_a,cur_b,d.front());
-                return;
+                grid[cur_a][cur_b] = true;
+                printf("%d %d %c LOST\n",cur_a,cur_b,d.front()); return;
             } else { // valid coordinates
                 cur_a = coor.first;
                 cur_b = coor.second;
             }
         }
     }
-    printf("%d %d %c",cur_a,cur_b,d.front());
+    printf("%d %d %c\n",cur_a,cur_b,d.front());
 }
 
 int main(int argc, char *argv[]) {
@@ -68,9 +68,9 @@ int main(int argc, char *argv[]) {
     char dir;
     string s;
     while(scanf("%d %d",&N,&M) != EOF) {
-        vector<vector<bool>> grid(N,vector<bool>(M));
+        vector<vector<bool>> grid(N+1,vector<bool>(M+1));
         while(scanf("%d %d %c",&start_a,&start_b,&dir) != EOF) {
-            getline(cin,s);
+            cin >> s;
             while(nextRotation(d,'L') != dir) {}
             endingPosition(s,dir,start_a,start_b,N,M,grid);
         }
--- 4-graph/graph-traversal/test.cc
+++ 4-graph/graph-traversal/test.cc
@@ -5,6 +5,35 @@
 #include <unordered_set>
 using namespace std;
 
+void mutant() {
+    freopen("input/mutant-03.txt","w+",stdout);
+    srand(time(NULL));
+    // largest dimensions
+    int N = 50, M = 50;
+    printf("%d %d\n",N,M);
+    for(int i = 0; i < 20; i++) {
+        // random starting position
+        int x = rand() % 50 + 1;
+        int y = rand() % 50 + 1;
+        int c = rand() % 4 + 1;
+        char dir;
+        if(c == 1) dir = 'N'; if(c == 2) dir = 'E'; 
+        if(c == 3) dir = 'S'; if(c == 4) dir = 'W';
+        printf("%d %d %c\n",x,y,dir);
+
+        // random string
+        string s = "";
+        for(int i = 0; i < 100; i++) {
+            int num = rand() % 3 + 1;
+            if(num == 1) s += 'L';
+            if(num == 2) s += 'R';
+            if(num == 3) s += 'F';
+        }
+
+        printf("%s\n",s.c_str());
+    }
+}
+
 void random(int N) {
     freopen("input/forward-05.txt","w+",stdout);
     int TC = 20,u,v;
@@ -35,8 +64,9 @@ void large(int N) {
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) { cerr << "enter an input integer." << endl; return 0; }
-    int N = atoi(argv[1]);
-    large(N);
+    //if(argc < 2) { cerr << "enter an input integer." << endl; return 0; }
+    //int N = atoi(argv[1]);
+    //large(N);
+    mutant();
     return 0;
 }
