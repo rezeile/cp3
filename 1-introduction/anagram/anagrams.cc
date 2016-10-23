07f8f7f7843fe1ef5325c317d19937ab36c3420e
Eliezer Abate <code.eliezer@gmail.com>
2016-10-22 20:47:53 -0700

    added anagrams.cc

---
+++ 1-introduction/anagram/anagrams.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) { cerr << "enter an input file"; return -1; }
+    freopen(argv[1],"r",stdin);
+    return 0;
+}
