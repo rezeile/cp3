be98d43802bed941128ca1b649166d69a987c48a
Eliezer Abate <code.eliezer@gmail.com>
2016-10-26 11:21:36 -0700

    finished boiler plate code

--- 1-introduction/palindrome/magic-square.cc
+++ 1-introduction/palindrome/magic-square.cc
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <cstdio>
+#include <string>
 using namespace std;
+
+void magicSquare(string s) {
+
+}
 
 int main(int argc, char *argv[]) {
     if(argc < 2) { cerr << "enter an input file"; return -1; }
     freopen(argv[1],"r",stdin);
+    int TC;
+    cin >> TC; // 1 <= TC <= 60
+    string s;  // length(s) < 10K
+    for(int i = 0; i < TC; i++) {
+        getline(cin,s);
+        magicSquare(s);
+    }
     return 0;
 }
