cd93448247616fd0f5a6383433d2f4956da49ee0
Eliezer Abate <code.eliezer@gmail.com>
2016-10-26 08:07:20 -0700

    completed boilerplate function for knights

--- 4-graph/graph-traversal/knights.cc
+++ 4-graph/graph-traversal/knights.cc
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
+#include <utility> // for std::pair
 using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> mtx;
+typedef vector<pair<int,int>> vii;
+
+void findGridCells(int R,int C,int M,int N,vii &vp) {
+
+}
 
 int main(int argc, char *argv[]) {
     if(argc < 2) { cerr << "enter an input file"; return -1; }
@@ -10,10 +20,13 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i < TC; i++) {
         cin >> R >> C >> M >> N;
         cin >> W;
+        vii vp;
         for(int j = 0; j < W; j++) {
             cin >> a >> b;
-            printf("%d %d\n",a,b);
+            auto p = make_pair(a,b);
+            vp.push_back(p);
         }
+        findGridCells(R,C,M,N,vp);
     }
     return 0;
 }
