0839b2c77576bc749e82a5b3dc8d1ad7aab451db
Eliezer Abate <code.eliezer@gmail.com>
2016-10-27 21:07:35 -0700

    cleaned up bfs implementation to redo later

--- 4-graph/single-source-shortest-path/node-too-far.cc
+++ 4-graph/single-source-shortest-path/node-too-far.cc
@@ -16,42 +16,13 @@ int case_cnt = 1;
 
 map<int,bool> visited;
 
+
+
+/*** HELPER FUNCTIONS ****/
+
 void clearVisited() {
-    for(auto p: visited)
-        p.second = false;
-}
-
-void bfs(int node,int TTL,graph &g,int &reachable) {
-   // regular bfs
-   queue<pair<int,int>> q;
-   q.push(make_pair(node,TTL));
-   auto search = visited.find(node);
-   search->second = true;
-   while(!q.empty()) {
-       auto front = q.front();
-       int ttl = front.second;
-       front.second = INF;
-       q.pop();
-       vector<pair<int,int>> neighbors = (g.find(front.first))->second;
-       for(auto n : neighbors) {
-           search = visited.find(n.first);
-           if(!search->second) {
-               search->second = true;
-               n.second = ttl - 1;
-               if(n.second < 0) reachable--;
-               q.push(n);    
-           }
-       }
-   }
-}
-
-void testTTLReachability(graph &g,vector<pair<int,int>> &v) {
-    for(auto p : v) {
-        int reachable = g.size();
-        clearVisited();
-        bfs(p.first,p.second,g,reachable);
-        int unreachable = g.size() - reachable;
-        printf("Case %d: %d nodes not reachable from node %d with TTL = %d.\n",case_cnt++,unreachable,p.first,p.second);
+    for(auto it = visited.begin(); it != visited.end(); it++) {
+        it->second = false;
     }
 }
 
@@ -76,6 +47,36 @@ void mapInsert(graph &g,int key,int value) {
     }
 }
 
+void insertNetwork(graph &g,int NC) {
+    int n1,n2;
+    for(int i = 0; i < NC; i++) {
+        scanf("%d %d",&n1,&n2);
+        mapInsert(g,n1,n2);
+        mapInsert(g,n2,n1);
+        visited.insert(make_pair(n1,false)); 
+    }
+}
+
+void insertTTL(vector<pair<int,int>> &v ) {
+    int N,TTL;
+    while(scanf("%d %d",&N,&TTL) != EOF) {
+        if(N == 0 && TTL == 0) break;
+        auto p = make_pair(N,TTL);
+        v.push_back(p);
+    }
+}
+
+
+/*** SOLUTION FUNCTIONS  ***/
+void bfs(int node,int TTL,graph &g,int &unreachable) {
+
+}
+
+void testTTLReachability(graph &g,vector<pair<int,int>> &v) {
+
+}
+
+/*** MAIN ***/
 int main(int argc, char *argv[]) {
     if(argc < 2) { cerr << "enter an input file"; return -1; }
     freopen(argv[1],"r",stdin);
@@ -86,22 +87,11 @@ int main(int argc, char *argv[]) {
         if((NC = stoi(input)) == 0) break;
         graph g;
         visited.clear();
-        int n1,n2;
-        for(int i = 0; i < NC; i++) {
-            scanf("%d %d",&n1,&n2);
-            mapInsert(g,n1,n2);
-            mapInsert(g,n2,n1);
-            visited.insert(make_pair(n1,false)); 
-        }
-        int N, TTL;
+        insertNetwork(g,NC);
         vector<pair<int,int>> v;
-        while(scanf("%d %d",&N,&TTL) != EOF) {
-            if(N == 0 && TTL == 0) break;
-            auto p = make_pair(N,TTL);
-            v.push_back(p);
-        }
-        //printAList(g);
-        testTTLReachability(g,v);
+        insertTTL(v); 
+        printAList(g);
+        //testTTLReachability(g,v);
     }
     return 0;
 }
