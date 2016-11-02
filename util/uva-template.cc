e60e48b3cffe0c3d2c509595d752395ae65c438b
Eliezer Abate <code.eliezer@gmail.com>
2016-11-01 20:35:51 -0700

    modified fill bates

--- util/uva-template.cc
+++ util/uva-template.cc
@@ -1,24 +1,12 @@
 #include <cstdio>
 #include <iostream>
-#include <vector>
-#include <deque>
-#include <cmath>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <unordered_set>
-#include <stack>
-#include <queue>
-#include <list>
-#include <forward_list>
-#include <numeric>
-#include <algorithm>
+#include <string>
+#include <sstream>
 using namespace std;
 
 int main(int argc, char *argv[]) {
     if(argc < 2) { cerr << "enter an input file"; return -1; }
     freopen(argv[1],"r",stdin);
     string input;
-    stringstream ss;
     return 0;
 }
