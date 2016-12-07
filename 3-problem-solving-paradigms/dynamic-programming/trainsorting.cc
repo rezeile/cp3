80ebdc6f9c37cdc56f0c1a333c19fd9876321cfd
Eliezer Abate <code.eliezer@gmail.com>
2016-12-07 10:58:43 -0800

    filled comments for day 2

--- 3-problem-solving-paradigms/dynamic-programming/trainsorting.cc
+++ 3-problem-solving-paradigms/dynamic-programming/trainsorting.cc
@@ -2,6 +2,25 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+
+/* Day 1 (12/6/2016) Comments
+ * -----------------------------------
+ * Played with the problem and understood it but not much progress
+ * in the way of discovering an insight that will lead to a 
+ * solution.
+ */
+
+/* Day 2 (12/7/2016) Comments
+ * -----------------------------------
+ * Not much has been made in the way actually coding up a solution 
+ * to this problem. I do feel like some progress has been made
+ * in our approach to the problem. The interesting approach has been
+ * considered today and that is to think the problem through
+ * the perspective of an element at a particular index i, considering,
+ * exactly which elements at indices (i+1) through n will "sandwhich"
+ * it.More thought will be given to this tomorrow. 
+ */
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
