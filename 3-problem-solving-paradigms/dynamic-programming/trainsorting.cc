7a991df6b97354f873335d8ee8fb967e3c72be4b
Eliezer Abate <code.eliezer@gmail.com>
2016-12-09 08:48:12 -0800

    day insights logged

--- 3-problem-solving-paradigms/dynamic-programming/trainsorting.cc
+++ 3-problem-solving-paradigms/dynamic-programming/trainsorting.cc
@@ -3,11 +3,24 @@
 #include <string>
 #include <sstream>
 
-/* Day 1 (12/6/2016) Comments
+/* Day 4 (12/9/2016) Comments
  * -----------------------------------
- * Played with the problem and understood it but not much progress
- * in the way of discovering an insight that will lead to a 
- * solution.
+ * Used the insight from yesterday to try to derive a recursive structure
+ * to the problem. New insight is to try to use two different lists: one 
+ * that will keep track of the longest sequence "including" the ith 
+ * digit and one the longest "without" it.
+ */
+
+/* Day 3 (12/8/2016) Comments
+ * -----------------------------------
+ * The insight from yesterday allowed us to see that we can decompose the 
+ * problem into a modified LIS and LDS problem but the resulting problem
+ * would need to perform a LIS/LDS for each of the input values increasing
+ * the run time to O(n^2lgn), which is not fast enough for our problem. 
+ * An alternative approach was discovered with about 5 minutes left which 
+ * we will explore tomorrow and that involves an approach similar 
+ * to the Longest Common Subsequence problem where a solution was derived
+ * from working backwards.
  */
 
 /* Day 2 (12/7/2016) Comments
@@ -21,16 +34,11 @@
  * it.More thought will be given to this tomorrow. 
  */
 
-/* Day 3 (12/8/2016) Comments
+/* Day 1 (12/6/2016) Comments
  * -----------------------------------
- * The insight from yesterday allowed us to see that we can decompose the 
- * problem into a modified LIS and LDS problem but the resulting problem
- * would need to perform a LIS/LDS for each of the input values increasing
- * the run time to O(n^2lgn), which is not fast enough for our problem. 
- * An alternative approach was discovered with about 5 minutes left which 
- * we will explore tomorrow and that involves an approach similar 
- * to the Longest Common Subsequence problem where a solution was derived
- * from working backwards.
+ * Played with the problem and understood it but not much progress
+ * in the way of discovering an insight that will lead to a 
+ * solution.
  */
 
 using namespace std;
