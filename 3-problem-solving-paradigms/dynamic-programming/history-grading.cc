e2a61a0f46e3cea251def8c489737e2ed6d08b67
Eliezer Abate <code.eliezer@gmail.com>
2016-12-22 07:29:07 -0800

    day 2 comments; still running into a little trouble understanding the sample output

--- 3-problem-solving-paradigms/dynamic-programming/history-grading.cc
+++ 3-problem-solving-paradigms/dynamic-programming/history-grading.cc
@@ -7,17 +7,47 @@
 using namespace std;
 
 /*
+ * Day 2 Comments: 12/22/2016
+ * --------------------------
+ * I am still trying to understand why 2 of the sample answers
+ * are the way they are. This does not seem to be just a straighforward
+ * Longest Increasing Subsequence problem.
+ */
+
+/*
  * Day 1 Comments: 12/21/2016
  * --------------------------
  * Understood the Problem.
  */
 
-void printPartialCredit(vector<int> &answer,vector<int> &response) {
-    for(int i = 0; i < response.size(); i++) {
-        if(i == response.size() - 1) {
-            printf("%d\n",response[i]);
-        } else printf("%d ",response[i]);
+void printVector(vector<int> &vec) {
+    for(int i = 0; i < vec.size(); i++) {
+        if(i == vec.size() - 1) {
+            printf("%d\n",vec[i]);
+        } else printf("%d ",vec[i]);
     }
+}
+
+void printPartialCredit(vector<int> &answer,vector<int> &response) {
+    vector<int> ordering(answer.size() + 1);
+    vector<int> pc(response.size(),1);
+    int index = 0;
+    for(int i = 0; i < answer.size(); i++) {
+        ordering[answer[i]] = index++; 
+    }
+    int max = 1;
+    for(int i = 0; i < response.size(); i++) {
+        for(int j = 0; j < i; j++) {
+            if(ordering[response[i]] > ordering[response[j]]) {
+                if(pc[i] < pc[j] + 1) {
+                    pc[i] = pc[j] + 1;
+                    if(pc[i] > max) max = pc[i];
+                }
+            }
+        }
+    }
+    //printVector(pc);
+    printf("%d\n",max);
 }
 
 int main(int argc, char *argv[]) {
@@ -33,13 +63,14 @@ int main(int argc, char *argv[]) {
         for(int i = 0; i < N; i++) 
             ss >> v[i];
         while(getline(cin,input)) {
+            if(input.empty()) break;
             if(input.size() <= 2) { N = stoi(input); break; }
             vector<int> v2(N);
             stringstream ss2(input);
             for(int i = 0; i < N; i++) {
                 ss2 >> v2[i];
             }
-            //printPartialCredit(v,v2);
+            printPartialCredit(v,v2);
         }
     }
     return 0;
