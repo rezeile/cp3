5eb0545e035995c2c534614be2572caa6f702dcc
Eliezer Abate <code.eliezer@gmail.com>
2017-01-10 08:13:05 -0800

    babylon day 2, made some progress on it

--- 3-problem-solving-paradigms/dynamic-programming/babylon.cc
+++ 3-problem-solving-paradigms/dynamic-programming/babylon.cc
@@ -3,14 +3,20 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <set>
 using namespace std;
 
-int babylon(vector<int> &v) {
-    /*for(int i = 0; i < v.size(); i++) {
-        if(i != v.size() - 1) printf("%d ",v[i]);
-        else printf("%d\n",v[i]);
-    }*/
+int babylon(set<tuple<int,int,int>> &s) {
+    vector<tuple<int,int,int>> v(s.begin(),s.end());
+    int max_height = 0;
+    
     return 8;
+}
+
+void addToSet(int x,int y,int z,set<tuple<int,int,int>> &s) {
+    tuple<int,int,int> t1(x,y,z); s.insert(t1);
+    tuple<int,int,int> t2(z,x,y); s.insert(t2);
+    tuple<int,int,int> t3(y,z,x); s.insert(t3);
 }
 
 int main(int argc, char *argv[]) {
@@ -25,14 +31,16 @@ int main(int argc, char *argv[]) {
         if(TC == 0) break;
         int max = 0;
         vector<int> v(3);
+        set<tuple<int,int,int>> s;
+        s.clear();
         for(int i = 0; i < TC; i++) {
             string input;
             getline(cin,input);
             stringstream ss(input);
-            ss >> v[0] >> v[1] >> v[2];
-            int nmax = babylon(v);
-            if(nmax > max) max = nmax;
+            int x,y,z;
+            ss >> x >> y >> z;
         }
+        max = babylon(s);
         printf("Case %d: maximum height = %d\n",CN++,max);
     }
     return 0;
