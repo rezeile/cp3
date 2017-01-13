778f93fb2f2bd7ba246e6078f98d95f24048dae7
Eliezer Abate <code.eliezer@gmail.com>
2017-01-13 04:58:21 -0800

    babylon code getting too clunky; will see hints section tomorrow

--- 3-problem-solving-paradigms/dynamic-programming/babylon.cc
+++ 3-problem-solving-paradigms/dynamic-programming/babylon.cc
@@ -7,10 +7,24 @@
 #include <utility> // for pair
 using namespace std;
 
+enum relation {
+    IS_SMALLER,
+    IS_LARGER,
+    NO_RELATION
+};
 
+class Compare {
+    public:
+    bool operator() (const tuple<int,int,int> &t1,const tuple<int,int,int> &t2) const {
+         return get<0>(t1) < get<0>(t2) && get<1>(t1) < get<1>(t2);
+    }
+};
 // custom comparator 
-bool cmp(tuple<int,int,int> &t1,tuple<int,int,int> &t2) {
-    return get<0>(t1) < get<0>(t2) && get<1>(t2) < get<1>(t2);
+enum relation cmp(tuple<int,int,int> &t1,tuple<int,int,int> &t2) {
+    //printf("t1[0] = %d < t2[0] = %d && t1[1] = %d < t2[1] = %d\n",get<0>(t1),get<0>(t2),get<1>(t1), get<1>(t2));
+    if(get<0>(t1) > get<0>(t2) && get<1>(t1) > get<1>(t2)) return IS_LARGER;
+    else if (get<0>(t1) > get<0>(t2) && get<1>(t1) > get<1>(t2)) return IS_SMALLER;
+    return NO_RELATION;
 }
 
 // test final state of L
@@ -19,6 +33,18 @@ void printL(vector<pair<int,int>> &L) {
         if(i != L.size() - 1) printf("(%d,%d) ",L[i].first,L[i].second);
         else printf("(%d,%d)\n",L[i].first,L[i].second);
     }
+}
+
+// tests
+
+/* test set */
+
+void babylonSetTest(set<tuple<int,int,int>> &s) {
+   vector<tuple<int,int,int>> v(s.begin(),s.end());
+   for(int i = 0; i < v.size(); i++) {
+        if(i != v.size() - 1) printf("(%d,%d,%d), ",get<0>(v[i]),get<1>(v[i]),get<2>(v[i]));
+        else printf("(%d,%d,%d)\n",get<0>(v[i]),get<1>(v[i]),get<2>(v[i]));
+   }
 }
 
 int babylon(set<tuple<int,int,int>> &s) {
@@ -30,14 +56,18 @@ int babylon(set<tuple<int,int,int>> &s) {
         L[i].second = get<2>(ti); // get height 
         for(int j = 0; j < i; j++) {
             tuple<int,int,int> tj = v[j];
-            if(cmp(ti,tj)) {
-                L[i].first += 1;
+            if(cmp(ti,tj) == IS_SMALLER) {
+                L[i].first += L[j].first;
                 L[i].second += L[j].second;
                 if(L[i].second > maxHeight) maxHeight = L[i].second;
+            } else if(cmp(ti,tj) == IS_LARGER) {
+                L[j].first += L[i].first;
+                L[j].second += L[j].second;
+                if(L[i].second > maxHeight) maxHeight = L[j].second;
             }
         }
     }
-    printL(L);
+    //printL(L);
     return maxHeight;
 }
 
@@ -47,16 +77,6 @@ void addToSet(int x,int y,int z,set<tuple<int,int,int>> &s) {
     tuple<int,int,int> t3(y,z,x); s.insert(t3);
 }
 
-// tests
-
-/* test set */
-void babylonSetTest(set<tuple<int,int,int>> &s) {
-   vector<tuple<int,int,int>> v(s.begin(),s.end());
-   for(int i = 0; i < v.size(); i++) {
-        if(i != v.size() - 1) printf("(%d,%d,%d), ",get<0>(v[i]),get<1>(v[i]),get<2>(v[i]));
-        else printf("(%d,%d,%d)\n",get<0>(v[i]),get<1>(v[i]),get<2>(v[i]));
-   }
-}
 
 int main(int argc, char *argv[]) {
     if(argc < 2) { cerr << "enter an input file"; return -1; }
@@ -82,7 +102,7 @@ int main(int argc, char *argv[]) {
         }
         //babylonSetTest(s);
         max = babylon(s);
-        //printf("Case %d: maximum height = %d\n",CN++,max);
+        printf("Case %d: maximum height = %d\n",CN++,max);
     }
     return 0;
 }
