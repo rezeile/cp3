2582e7294a98c8e8ed63388761b15fcb98678ba6
Eliezer Abate <code.eliezer@gmail.com>
2017-04-14 01:05:30 -0700

    completed clock hands

--- 1-introduction/time/clock-hands.cc
+++ 1-introduction/time/clock-hands.cc
@@ -12,11 +12,20 @@ void getDegrees(string &input) {
     string hour,min;
     getline(ss,hour,':');int hour_i = stoi(hour);
     getline(ss,min,':'); int min_i = stoi(min);
+    
+    double hour_d = hour_i * 30 + ((double)min_i/60) * 30;
+    int min_to_hour = min_i / 5;
+    if (min_to_hour == 0) min_to_hour = 12;
+    int min_rem = min_i % 5;
+    double min_d = min_to_hour * 30 + (min_rem) * 6;
+    double degrees = abs(hour_d - min_d);
+    if (degrees > 180.0) degrees = 360.0 - degrees;
+    printf("%.3f\n",degrees); 
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) { fprintf(stderr,"enter an input file"); return -1; }
-    freopen(argv[1],"r",stdin);
+    //if(argc < 2) { fprintf(stderr,"enter an input file"); return -1; }
+    //freopen(argv[1],"r",stdin);
     string input; 
     while (getline(cin,input)) {
         if (input == "0:00") break;
