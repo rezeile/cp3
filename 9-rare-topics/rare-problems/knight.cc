d47faa21e6fbb3a868adbb9d537e97eac2e47aa9
Eliezer Abate <code.eliezer@gmail.com>
2017-04-15 19:29:13 -0700

    added knightmoves refactored code

--- 9-rare-topics/rare-problems/knight.cc
+++ 9-rare-topics/rare-problems/knight.cc
@@ -31,23 +31,18 @@ vector<pair<string,int>> getNeighbors(string s,int level) {
         int a = 1 + i, b = 2 - i;
         char w = s[0] - a, x = s[1] - b;
         char y = s[0] + a, z = s[1] + b;
-        
         string s1; s1+= w; s1+= x;
-        if (okayToAdd(s1,level)) {
+        if (okayToAdd(s1,level)) 
             q.push_back(make_pair(s1,level));
-        }
         s1.clear(); s1 += w; s1 += z;
-        if (okayToAdd(s1,level)) {
+        if (okayToAdd(s1,level)) 
             q.push_back(make_pair(s1,level));
-        }
         s1.clear(); s1 += y; s1 += x;
-        if (okayToAdd(s1,level)) {
+        if (okayToAdd(s1,level)) 
             q.push_back(make_pair(s1,level));
-        }
         s1.clear(); s1 += y; s1 += z;
-        if (okayToAdd(s1,level)) {
+        if (okayToAdd(s1,level)) 
             q.push_back(make_pair(s1,level));
-        }
     }
     return q;
 }
@@ -55,7 +50,7 @@ vector<pair<string,int>> getNeighbors(string s,int level) {
 void knightMoves(string s) {
     queue<pair<string,int>> q;
     q.push(make_pair(s,0));
-
+    
     while (!q.empty()) {
         pair<string,int> p = q.front(); q.pop();
         visited[p.first[0]-'a'][p.first[1]-'1'] = p.second;
@@ -65,13 +60,9 @@ void knightMoves(string s) {
             q.push(N[i]);
         }
     }
-
 }   
 
 int main(int argc, char *argv[]) {
-    //if(argc < 2) { fprintf(stderr,"enter an input file"); return -1; }
-    //freopen(argv[1],"r",stdin);
-    
     string input,a,b;
     while (getline(cin,input)) {
         stringstream ss(input);
