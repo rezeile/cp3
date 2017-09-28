ab069b79b1c51d4d40ef568bb9ab677b0955c4b6
Eliezer Abate <code.eliezer@gmail.com>
2017-09-27 17:06:24 -0700

    [string-processing - dp] completed prince and princess O(nlogn) LIS/LCS problem

--- 6-string-processing/string-dp/prince.cc
+++ 6-string-processing/string-dp/prince.cc
@@ -1,67 +1,81 @@
+#include <algorithm>
 #include <cstdio>
 #include <vector>
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 #include <utility>
+#include <unordered_map>
+#include <set>
 #include <iterator>
+#include <deque>
 using namespace std;
+
 
 using vi = vector<int>;
 using uint = unsigned int;
 int lineCounter = 0;
 
-uint getIndex(int i,int j,int n) {
-  return (uint) i * n + j; 
+void printVector(vi &v) {
+  if (v.empty()) cout << "[]" << endl;
+  else {
+    printf("%d",v[0]);
+    for(int i = 1; i < v.size(); i++) {
+      printf(" %d",v[i]);
+    }
+    printf("\n");
+  }
 }
 
-void solve(vi &s1, vi &s2,int n) {
-  unordered_map<uint,int> map;
-  unordered_map<uint,int>::iterator it;
-  int res = 0;
-  if (s1.empty() || s2.empty()) 
-    res = 0;
-  else {
-    for(int i = 0; i < s1.size(); i++) {
-      for(int j = 0; j < s2.size(); j++) {
-        //printf("s1[%d] = %d, s2[%d] = %d\n",i,s1[i],j,s2[j]);
-        //printf("index = %u\n",getIndex(i,j,n));
-        int val = 0, val2 = 0;
-        if (s1[i] != s2[j]) {
-          if (i > 0) {
-            uint index = getIndex(i-1,j,n);
-            it = map.find(index);
-            val = it->second;
-          }
-          if (j > 0) {
-            uint index = getIndex(i,j-1,n);
-            it = map.find(index);
-            val2 = it->second;
-          }
-          val = max(val,val2);
-          uint index = getIndex(i,j,n); 
-          map.insert(make_pair(index,val));
-        } else {
-          if (i > 0 && j > 0) {
-            uint index = getIndex(i-1,j-1,n); 
-            it = map.find(index);
-            val = it->second;
-          }
-          uint index = getIndex(i,j,n);
-          map.insert(make_pair(index,val + 1));
-        }
-      }
-    }
-    uint index = getIndex(s1.size()-1,s2.size()-1,n);
-    it = map.find(index);
-    res = it->second;
+void solve(vi &v1, vi &v2,int n) {
+  // find common set 
+  set<int> ref;
+  vector<int> target;
+  if (v1.size() < v2.size()) {
+    ref = set<int>(v1.begin(),v1.end());
+    target = v2;
+  } else {
+    ref = set<int>(v2.begin(),v2.end());
+    target = v1;
+  }
+
+  set<int> common;
+  for(auto &i: target) {
+    if (ref.find(i) != ref.end()) common.insert(i);
+  }
+  // trim vectors 
+  vector<int> nv1, nv2;
+  for(int i = 0; i < v1.size(); i++) {
+    if (common.find(v1[i]) != common.end()) nv1.push_back(v1[i]);
   }
   
-  cout << "Case " << ++lineCounter << ": " << res << endl;
+  unordered_map<int,int> map;
+  for(int i = 0; i < v2.size(); i++) {
+    if (common.find(v2[i]) != common.end()) {
+      nv2.push_back(v2[i]);
+      map.insert(make_pair(v2[i],nv2.size()-1)); 
+    }
+  }
+
+  // run LIS on index vector
+  vector<int> index_vector;
+  for(int i = 0; i <  nv1.size(); i++) {
+    index_vector.push_back(map.find(nv1[i])->second);
+  }
+  
+  vector<int> lis;
+  for(int i = 0; i < index_vector.size(); i++) {
+    int index = lower_bound(lis.begin(),lis.end(),index_vector[i]) - lis.begin();
+    if (index <= 0 && lis.empty()) lis.push_back(index_vector[i]);
+    else if  (index >= lis.size()) lis.push_back(index_vector[i]);
+    else if (lis[index] > index_vector[i]) lis[index] = index_vector[i];
+  }
+ 
+  printf("Case %d: %d\n",++lineCounter,(int) lis.size());
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) { fprintf(stderr,"enter an input file"); return -1; }
-    freopen(argv[1],"r",stdin);
+    //if(argc < 2) { fprintf(stderr,"enter an input file"); return -1; }
+    //freopen(argv[1],"r",stdin);
     int T,n,p,q, c; scanf("%d",&T);
     for(int i = 0; i < T; i++) {
        scanf("%d",&n); scanf("%d",&p); scanf("%d",&q);
