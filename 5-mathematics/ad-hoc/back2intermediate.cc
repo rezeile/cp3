91997981edda1f54bbb5060af06a8dd3a415a9bc
Eliezer Abate <eliezerabate@fb.com>
2018-02-06 07:01:42 -0800

    added uva-node template; edited makefile

---
+++ 5-mathematics/ad-hoc/back2intermediate.cc
@@ -0,0 +1,8 @@
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) { fprintf(stderr,"enter an input file"); return -1; }
+    freopen(argv[1],"r",stdin);
+    return 0;
+}
