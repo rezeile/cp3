a727979a1f31c6532ca469f45221c7a22067c266
Eliezer Abate <eliezerabate@fb.com>
2018-08-03 08:30:25 -0700

    [math][adhoc] - wrote problem statement for ant.cc

--- 5-mathematics/ad-hoc/ant.cc
+++ 5-mathematics/ad-hoc/ant.cc
@@ -2,6 +2,21 @@
 #include <iostream>
 using namespace std;
 
+/* 
+ * Problem Statement 
+ * -----------------------------
+ *
+ * starting at corner (1,1) on a chess board, an ant travels in a snake like path. 
+ * first it travels:
+ *   north to (1,2) 
+ *   east to (2,2)
+ *   south to (2,1) 
+ *   east (3,1)
+ *   north to (3,2), 
+ *   ...
+ *
+ */
+
 void solve(int N) {
   int c = 2, r = 3;;
    
